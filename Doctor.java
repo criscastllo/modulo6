202752a59b66ed8995dc99db9c73b9f4cb1a3966
criscastllo <101481228+criscastllo@users.noreply.github.com>
2022-03-12 17:34:19 -0600

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author Cristian Castillo
+ */
+public class Doctor extends Persona1{
+
+    /**
+     *
+     * @return
+     */
+    
+    public String getPersona(){
+    return "Nombre: Reniery Salvador Castillo";}
+    public String getDeportista(){
+    return "Profesión: Doctor";} 
+
+}
---
+++ Persona1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author Cristian Castillo
+ */
+public abstract class Persona1 {
+       
+ abstract String getPersona();
+ abstract String getDeportista();
+
+}
